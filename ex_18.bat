a6e117145b429b0be1043afb66f4c6894a814e16
SamuelPapyan <samuel.papyan170@gmail.com>
2025-10-09 15:27:51 +0400

    ex18 modified

--- ex_18.bat
+++ ex_18.bat
@@ -10,16 +10,18 @@ set directory=
 for %%i in (%CD%) do (
     set directory=%%i
 )
-if !directory! EQU C:\ (
+echo %directory%
+
+if %directory% == C:\ (
     goto :end
 )
 if exist %file% (
-    echo "File is in !directory!"
+    echo "File is in %directory%"
 ) else (
     goto :moveback
 )
 
 :end
-echo "There is no file %file% in the directory"
+echo "There is no file %file% in the filesystem"
 
 endlocal
